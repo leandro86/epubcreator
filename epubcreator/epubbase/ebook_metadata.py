e147075debeaa2cf1580b5748066084baf021b7c
Leandro <lea.ciancio@gmail.com>
2014-03-30 15:00:36 -0300

    ...

--- epubcreator/epubbase/ebook_metadata.py
+++ epubcreator/epubbase/ebook_metadata.py
@@ -22,7 +22,7 @@ class Metadata:
                           "seas, Señor, Padre que estás en lo alto! Todo esto me han urdido mis enemigos malvados».\n"
                           '<p class="salto05">A<small>NÓNIMO</small></p>')
 
-    DEFAULT_AUTHOR_BIOGRAPHY = ("NOMBRE DEL AUTOR (Reikiavik, Islandia, 2013 - Terra III, 3096). Lorem ipsum dolor sit amet, consectetur "
+    DEFAULT_AUTHOR_BIOGRAPHY = ("NOMBRE DEL AUTOR (Reikiavik, Islandia, 2013 - Terra III, 3072). Lorem ipsum dolor sit amet, consectetur "
                                 "adipiscing elit. Nunc vel libero sed est ultrices elementum at vel lacus. Sed laoreet, velit nec congue "
                                 "pellentesque, quam urna pretium nunc, et ultrices nulla lacus non libero.\n"
                                 "Integer eu leo justo, vel sodales arcu. Donec posuere nunc in lectus laoreet a rhoncus enim fermentum. "
